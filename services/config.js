35aa452a14743e5805e1cc5c53651f71497a9f77
yuxinran0917 <1828179225@qq.com>
2026-07-09 15:32:56 +0800

    完善藏品图鉴云端图片展示

--- services/config.js
+++ services/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   useBackend: true,
   useStageBackend: true,
-  apiBaseUrl: 'http://127.0.0.1:3000/api',
+  apiBaseUrl: 'https://fan-accounting-api-279639-5-1451816515.sh.run.tcloudbase.com/api',
   userId: 'local-user'
 };
